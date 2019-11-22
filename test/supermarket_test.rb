fbfe1ebaf33a70290a601ef598d7fc71a50df8a2
Tony Griffin <tony@madetech.com>
2019-11-22 14:46:31 +0000

    TCR!

--- test/supermarket_test.rb
+++ test/supermarket_test.rb
@@ -45,9 +45,7 @@ class SupermarketTest < Minitest::Test
 
   def test_buy_five_get_one_free 
     5.times { @the_cart.add_item(@toothbrush) }
-    @teller.add_special_offer(Kata::SpecialOfferType::THREE_FOR_TWO, @toothbrush, @catalog.unit_price(@toothbrush))
-    receipt = @teller.checks_out_articles_from(@the_cart)
-    verify Kata::ReceiptPrinter.new(40).print_receipt(receipt)
+    verify Kata::ReceiptPrinter.new(40).print_receipt(discount_onReceipt_toothbrush(@the_cart))
   end
 
   def test_loose_weight_product
