e68ed8c3b20e4affe760d2cb13b23f567c0bbdeb
Craig J. Bass <craig@madetech.co.uk>
2019-11-22 16:10:32 +0000

    TCR!

--- lib/kata/models/offer.rb
+++ lib/kata/models/offer.rb
@@ -6,25 +6,15 @@ class Kata::Offer
     @argument = argument
     @product = product
   end
-
-  def divider
-    case offer_type
-    when Kata::SpecialOfferType::THREE_FOR_TWO
-      discount_divider = 3
-    when Kata::SpecialOfferType::TWO_FOR_AMOUNT
-      discount_divider = 2
-    when Kata::SpecialOfferType:: FIVE_FOR_AMOUNT
-      discount_divider = 5
-    else
-      discount_divider = 1
-    end
-  end
-
 end
 
 class Kata::ThreeForTwoOffer < Kata::Offer
   def initialize(product, argument)
     super('THREE_FOR_TWO', product, argument)
+  end
+
+  def divider
+    3
   end
 end
 
@@ -32,16 +22,28 @@ class Kata::TenPercentDiscountOffer < Kata::Offer
   def initialize(product, argument)
     super('TEN_PERCENT_DISCOUNT', product, argument)
   end
+
+  def divider
+    1
+  end
 end
 
 class Kata::TwoForAmountOffer < Kata::Offer
   def initialize(product, argument)
     super('TWO_FOR_AMOUNT', product, argument)
   end
+
+  def divider
+    2
+  end
 end
 
 class Kata::FiveForAmountOffer < Kata::Offer
   def initialize(product, argument)
     super('FIVE_FOR_AMOUNT', product, argument)
+  end
+
+  def divider
+    5
   end
 end
