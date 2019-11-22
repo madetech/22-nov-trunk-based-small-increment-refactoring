89d7d60a158968bb3674eaeced8214f0f5993a97
Maria Astakhova <maria@madetech.com>
2019-11-22 15:04:53 +0000

    tcr

--- lib/kata/models/teller.rb
+++ lib/kata/models/teller.rb
@@ -12,11 +12,11 @@ class Kata::Teller
   def checks_out_articles_from(the_cart)
     receipt = Kata::Receipt.new
     for item in the_cart.items do
-      product = item.product
+      p = item.product
       quantity = item.quantity
       unit_price = @catalog.unit_price(p)
       price = quantity * unit_price
-      receipt.add_product(product, quantity, unit_price, price)
+      receipt.add_product(p, quantity, unit_price, price)
     end
     the_cart.handle_offers(receipt, @offers, @catalog)
 
